324ba53815ab8bf08d4cacf7d191c68269d576bd
manobra-cloud <manobra.cloud@gmail.com>
2026-05-15 12:58:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
-app.use("/files", express.static("."));
+app.use("/files", express.static(process.cwd()));
 
 app.get("/", (req, res) => {
   res.send("API DOCX attiva");
